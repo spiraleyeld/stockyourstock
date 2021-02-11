a55f914dc32e3cd47e6bc1b2b5accb50d91f288d
awei.lin <spiraleyeld@gmail.com>
2021-02-11 23:31:43 +0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <stocktable></stocktable>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import stocktable from './components/stocktable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    stocktable
   }
 }
 </script>
---
+++ src/components/stocktable.vue
@@ -0,0 +1,25 @@
+<template>
+    <h2 class="blue">Stock Good Info {{ msg }}</h2>
+</template>
+<script>
+export default {
+    name:'StockGoodNews',
+    props:{
+        msg:{
+            type:String,
+            default: "ALL in Here !!!"
+        }
+    },
+    methods:{
+    },
+    data(){
+        return{
+        }
+    },
+}
+</script>
+<style>
+.blue {
+    color:skyblue;
+}
+</style>
